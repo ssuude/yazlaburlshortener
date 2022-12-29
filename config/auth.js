119079af73c479675d350d825a883ac20ba20eb1
Mert GÖKYAR <gokyarmert01@gmail.com>
2022-12-29 19:11:05 +0300

    frontend

--- config/auth.js
+++ config/auth.js
@@ -3,14 +3,14 @@ module.exports = {
       if (req.isAuthenticated()) {
         return next();
       }
-      req.flash('error_msg', 'Please log in to view that resource');
+      req.flash('error_msg', 'Bu kaynağı görüntülemek için lütfen giriş yapın');
       res.redirect('/users/login');
     },
     ensureAuthenticatedAdmin: function(req, res, next) {
       if (req.isAuthenticated() && req.user.isAdmin) {
         return next();
       }
-      req.flash('error_msg', 'Please log in with a valid admin account!');
+      req.flash('error_msg', 'Lütfen geçerli bir yönetici hesabıyla oturum açın');
       res.redirect('/users/login');
     },
     forwardAuthenticated: function(req, res, next) {
